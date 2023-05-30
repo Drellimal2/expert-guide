4ebbfb4ba083d3bcee5a0bfb518b2b0b19f25cdc
Dane Miller <dmiller2310@hotmail.com>
2023-05-30 19:36:45 +0900

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Dane Miller on 2023/05/30.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
